d117a352d5d9a5ecc1036bad38755729c723fa6d
Chris Horner <chris@horner.chat>
2021-03-18 22:16:08 +1100

    Create edge-to-edge hello world

--- app/src/main/java/codes/chrishorner/weatherchallenge/MainActivity.kt
+++ app/src/main/java/codes/chrishorner/weatherchallenge/MainActivity.kt
@@ -18,44 +18,23 @@ package codes.chrishorner.weatherchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import codes.chrishorner.weatherchallenge.style.WeatherChallengeTheme
+import dev.chrisbanes.accompanist.insets.ProvideWindowInsets
 
 class MainActivity : AppCompatActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    // Render under the status and navigation bars.
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+
     setContent {
-      WeatherChallengeTheme {
-        App()
+      ProvideWindowInsets {
+        WeatherChallengeTheme {
+          HomeScreen()
+        }
       }
     }
-  }
-}
-
-// Start building your app here!
-@Composable
-fun App() {
-  Surface(color = MaterialTheme.colors.background) {
-    Text(text = "Ready... Set... GO!")
-  }
-}
-
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun LightPreview() {
-  WeatherChallengeTheme {
-    App()
-  }
-}
-
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-  WeatherChallengeTheme(darkTheme = true) {
-    App()
   }
 }
